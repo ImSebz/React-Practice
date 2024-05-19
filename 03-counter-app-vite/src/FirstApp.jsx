e4e791bcd5aad89eee8a20692fe29858e21b4482
Sebastian <juansecapri@hotmail.com>
2024-05-19 18:09:21 -0500

    Avances sección 4

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,17 @@
+// const App = () => {
+//     //documen.createElement('h1')
+//     return (
+//         <h1>Hello World!</h1>
+//     );
+// }
+
+// export default App;
+
+
+export const FirstApp = () => {
+    //documen.createElement('h1')
+    return (
+        <h1>First App</h1>
+    );
+}
+
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { FirstApp } from './FirstApp';
+import { SecondApp } from './SecondApp'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <FirstApp />
+        <SecondApp />
+    </React.StrictMode>
+);
